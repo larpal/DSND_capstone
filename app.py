ca205ef6247cb3310c8cc97b87815a44e159ac14
larpal <lars.palzer@gmail.com>
2020-05-21 20:07:55 +0200

    Added comments to app.py

--- app.py
+++ app.py
@@ -10,18 +10,18 @@ from tabulate import tabulate
 from etl import etl_main
 
 
-#data_wrangling.init()
 
-#data_wrangling.df_ctr_cum
-#
 st.title('COVID-19 Dashboard for Germany')
 
+# run ETL pipeline
+# note that the data is updated once per day, so the data pipeline is
+# cached using st.cache and only needs to be rerun once per day.
 df_deaths_stats, df_stats, df_cases_roll,\
         df_ctr, df_ctr_cum, df_sta, df_sta_cum,\
         df_lkr, df_lkr_cum, df_cases_loc_long\
         = etl_main()
 
-
+# navigation sidebar
 nav_country_map = 'Country Map'
 nav_country = 'Country Stats'
 nav_states = 'States'
@@ -30,8 +30,8 @@ site_navigation = \
     st.sidebar.radio('Site Navigation',\
                             (nav_country,nav_country_map,nav_states,nav_districts))
 
+# run chosen subpage
 if site_navigation == nav_country:
-    #country_main(df_ctr_cum,df_ctr,_width,_height)
     country.main(df_stats, df_deaths_stats, df_ctr, df_ctr_cum)
 elif site_navigation == nav_country_map:
     country.show_map(df_cases_loc_long)
@@ -39,5 +39,3 @@ elif site_navigation == nav_states:
     states.main(df_sta, df_sta_cum)
 elif site_navigation == nav_districts:
     landkreis.main(df_lkr, df_cases_roll, df_cases_loc_long)
-    pass
-#
